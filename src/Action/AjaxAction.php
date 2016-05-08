8a3b29a3de65ad1ae6eec44a3a3a912f1811257a
Gauthier Delamarre <gauthier@objective-php.org>
2016-05-08 11:27:48 +0200

    Clean AjaxAction

--- src/Action/AjaxAction.php
+++ src/Action/AjaxAction.php
@@ -3,9 +3,6 @@
 namespace ObjectivePHP\Application\Action;
 
 
-use ObjectivePHP\Application\Action\HttpAction;
-use ObjectivePHP\Application\Action\SubRoutingAction;
-use ObjectivePHP\Application\Middleware\AbstractMiddleware;
 use Zend\Diactoros\Response\JsonResponse;
 
 /**
@@ -26,27 +23,5 @@ abstract class AjaxAction extends HttpAction
         return ($response instanceof JsonResponse) ? $response : new JsonResponse($response);
     }
 
-    /**
-     * @return mixed
-     */
-    public function route()
-    {
-        $verb = $this->getApplication()->getRequest()->getMethod();
-
-        return strtolower($verb);
-
-    }
-
-    /**
-     * @param $reference
-     * @return array
-     */
-    public function getMiddleware($reference)
-    {
-        if(method_exists($this, $reference))
-        {
-            return [$this, $reference];
-        }
-    }
 
 }
